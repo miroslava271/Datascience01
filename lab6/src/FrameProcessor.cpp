0e2a284d4d96078711cf71c2e23c370f54db44fc
miroslava271 <145108203+miroslava271@users.noreply.github.com>
2026-04-27 01:37:46 -0700

    Create FrameProcessor.cpp

---
+++ lab6/src/FrameProcessor.cpp
@@ -0,0 +1,58 @@
+#include "FrameProcessor.hpp"
+
+cv::Mat FrameProcessor::process(const cv::Mat& frame, KeyProcessor::Mode mode, >
+
+    cv::Mat result = frame.clone();
+
+    switch (mode) {
+
+        case KeyProcessor::GRAY:
+            cv::cvtColor(result, result, cv::COLOR_BGR2GRAY);
+            cv::cvtColor(result, result, cv::COLOR_GRAY2BGR);
+            break;
+
+        case KeyProcessor::BLUR:
+            cv::GaussianBlur(result, result, cv::Size(blurValue, blurValue), 0);
+            break;
+
+        case KeyProcessor::CANNY: {
+            cv::Mat gray;
+            cv::cvtColor(result, gray, cv::COLOR_BGR2GRAY);
+            cv::Canny(gray, result, 50, 150);
+            cv::cvtColor(result, result, cv::COLOR_GRAY2BGR);
+            break;
+        }
+
+        case KeyProcessor::INVERT:
+            cv::bitwise_not(result, result);
+            break;
+
+        case KeyProcessor::SOBEL: {
+            cv::Mat gray, gx, gy;
+            cv::cvtColor(result, gray, cv::COLOR_BGR2GRAY);
+
+            cv::Sobel(gray, gx, CV_16S, 1, 0);
+            cv::Sobel(gray, gy, CV_16S, 0, 1);
+
+            cv::convertScaleAbs(gx, gx);
+            cv::convertScaleAbs(gy, gy);
+
+            cv::addWeighted(gx, 0.5, gy, 0.5, 0, result);
+            cv::cvtColor(result, result, cv::COLOR_GRAY2BGR);
+            break;
+        }
+
+        case KeyProcessor::THRESHOLD: {
+            cv::Mat gray;
+            cv::cvtColor(result, gray, cv::COLOR_BGR2GRAY);
+            cv::threshold(gray, result, 127, 255, cv::THRESH_BINARY);
+            cv::cvtColor(result, result, cv::COLOR_GRAY2BGR);
+            break;
+        }
+
+        default:
+            break;
+    }
+
+    return result;
+}
